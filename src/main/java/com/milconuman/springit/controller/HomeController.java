4b2a5009a8086cc08d538fbc4e73677004b2671a
Milco Numan <milco.numan@gmail.com>
2021-04-20 20:44:45 +0200

    Shortcut to return template

--- src/main/java/com/milconuman/springit/controller/HomeController.java
+++ src/main/java/com/milconuman/springit/controller/HomeController.java
@@ -1,16 +1,15 @@
 package com.milconuman.springit.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+
+@Controller
 public class HomeController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @GetMapping(path = "/")
     public String home(){
-        return "Hello, cruel spring boot world!";
+        return "index";
     }
 
 }
