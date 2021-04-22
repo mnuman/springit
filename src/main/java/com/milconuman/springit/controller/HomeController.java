93ffc49f75e1f5ec6b8bb07853a9bd3153295428
Milco Numan <milco.numan@gmail.com>
2021-04-22 19:06:32 +0200

    Just before templating stuff ...

--- src/main/java/com/milconuman/springit/controller/HomeController.java
+++ src/main/java/com/milconuman/springit/controller/HomeController.java
@@ -9,11 +9,4 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
-
-    @GetMapping(path = "/")
-    public String home(Model model, HttpServletRequest request){
-        model.addAttribute("message", "Hello World!");
-        return "index";
-    }
-
 }
